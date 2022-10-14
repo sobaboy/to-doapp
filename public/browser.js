8944f77c9d44631ae9fbc4259e1da841946684f9
sobaboy <1214hso@naver.com>
2022-10-14 16:57:50 +0900

    todoapp

---
+++ public/browser.js
@@ -0,0 +1,78 @@
+function itemTemplate(item) {
+  return ` <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
+    <span class="item-text">${item.text}</span>
+    <div>
+      <button data-id="${item._id}" class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
+      <button data-id="${item._id}" class="delete-me btn btn-danger btn-sm">Delete</button>
+    </div>
+  </li>`;
+}
+
+// Initial Page Load Render
+let ourHTML = items
+  .map(function (item) {
+    return itemTemplate(item);
+  })
+  .join("");
+document.getElementById("item-list").insertAdjacentHTML("beforeend", ourHTML);
+// Create
+let createField = document.getElementById("create-field");
+document.getElementById("create-form").addEventListener("submit", function (e) {
+  e.preventDefault();
+  axios
+    .post("/create-item", {
+      text: createField.value,
+    })
+    .then(function (response) {
+      // Create the HTML for a new item
+      document
+        .getElementById("item-list")
+        .insertAdjacentHTML("beforeend", itemTemplate(response.data));
+      createField.value = "";
+      createField.focus();
+    })
+    .catch(function () {
+      console.log("나중에 다시 시도하세요");
+    });
+});
+
+document.addEventListener("click", function (e) {
+  // Delete
+  if (e.target.classList.contains("delete-me")) {
+    if (confirm("영구적으로 삭제하시겠습니까?")) {
+      axios
+        .post("/delete-item", {
+          id: e.target.getAttribute("data-id"),
+        })
+        .then(function () {
+          e.target.parentElement.parentElement.remove();
+        })
+        .catch(function () {
+          console.log("나중에 다시 시도하세요");
+        });
+    }
+  }
+  // Update
+  if (e.target.classList.contains("edit-me")) {
+    let userInput = prompt(
+      "수정 하시겠습니까?",
+      e.target.parentElement.parentElement.querySelector(".item-text").innerHTML
+    );
+
+    if (userInput) {
+      axios
+        .post("/update-item", {
+          text: userInput,
+          id: e.target.getAttribute("data-id"),
+        })
+        .then(function () {
+          e.target.parentElement.parentElement.querySelector(
+            ".item-text"
+          ).innerHTML = userInput;
+        })
+        .catch(function () {
+          console.log("나중에 다시 시도하세요");
+        });
+    }
+  }
+});
